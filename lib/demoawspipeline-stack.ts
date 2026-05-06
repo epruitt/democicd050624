ee2763ad053c562b0ff11336ee592ac06273c5b5
epruitt <epruitt223@gmail.com>
2026-05-06 00:36:40 -0500

    initial commit

---
+++ lib/demoawspipeline-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class DemoawspipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
