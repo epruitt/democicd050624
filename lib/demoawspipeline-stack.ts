41b1c19b99513cd496bacc2c93f5c496b91378a0
epruitt <epruitt223@gmail.com>
2026-05-06 00:47:13 -0500

    add code pipeline setup

--- lib/demoawspipeline-stack.ts
+++ lib/demoawspipeline-stack.ts
@@ -11,6 +11,13 @@ export class DemoawspipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // AWS CI/CD Pipelines
+    const pipeline = new CodePipeline(this, "demoPipeline", {
+      synth: new ShellStep("Synth", {
+        //use connection created using AWS console to authenticate to gitHub
+        input: CodePipelineSource.gitHub("epruitt/democicd050624", "main"),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
   }
 }
